3cc6307cfd15d6b137dbf0421978533c9d81f13c
Štěpán Zrník <stepan.zrnik@gmail.com>
2022-11-09 06:42:50 +0100

    test

--- tests/CnbExchangeTest.php
+++ tests/CnbExchangeTest.php
@@ -55,13 +55,35 @@ class CnbExchangeTest extends TestCase
             $GBP_263_13->getAmount()
         );
 
-        $PHP_100_00 = new Money(10000, new Currency("PHP"));
+        $this->exchange->setDate(
+            (new DateTimeImmutable())->setDate(2022, 11, 01)
+        );
 
-        $CZK_51_33 = $this->converter->convert($PHP_100_00, new Currency("CZK"));
+        $CZK_2500_00 = new Money(250000, new Currency("CZK"));
+
+        $EUR_102_10 = $this->converter->convert($CZK_2500_00, new Currency("EUR"));
 
         $this->assertEquals(
-            5133,
-            $CZK_51_33->getAmount()
+            10210,
+            $EUR_102_10->getAmount()
+        );
+
+        $PHP_100_00 = new Money(10000, new Currency("PHP"));
+
+        $CZK_42_37 = $this->converter->convert($PHP_100_00, new Currency("CZK"));
+
+        $this->assertEquals(
+            4237,
+            $CZK_42_37->getAmount()
+        );
+
+        $CZK_100_00 = new Money(10000, new Currency("CZK"));
+
+        $PHP_236_00 = $this->converter->convert($CZK_100_00, new Currency("PHP"));
+
+        $this->assertEquals(
+            23600,
+            $PHP_236_00->getAmount()
         );
     }
 
